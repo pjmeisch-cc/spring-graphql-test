7e96f7d078de724856ad75e4717788e7cb515ae4
Peter-Josef Meisch <peter-josef.meisch@codecentric.de>
2024-06-14 10:43:27 +0200

    set trace id if none came in

--- src/main/kotlin/com/porsche/slfinprovd/springgraphqltest/graphql/RequestHeaderInterceptor.kt
+++ src/main/kotlin/com/porsche/slfinprovd/springgraphqltest/graphql/RequestHeaderInterceptor.kt
@@ -1,6 +1,7 @@
 package com.porsche.slfinprovd.springgraphqltest.graphql
 
 import com.porsche.slfinprovd.springgraphqltest.HelloService
+import com.porsche.slfinprovd.springgraphqltest.common.createUniqueTraceId
 import org.springframework.graphql.server.WebGraphQlInterceptor
 import org.springframework.graphql.server.WebGraphQlRequest
 import org.springframework.graphql.server.WebGraphQlResponse
@@ -15,15 +16,16 @@ class RequestHeaderInterceptor(
         request: WebGraphQlRequest,
         chain: WebGraphQlInterceptor.Chain,
     ): Mono<WebGraphQlResponse> {
-        var traceId = request.headers.getFirst(TRACE_ID)
+        var traceId = request.headers.getFirst(TRACE_ID) ?: createUniqueTraceId("graphql-request")
 
         request.configureExecutionInput { _, builder ->
-            builder.graphQLContext(
-                mapOf(
-                    TRACE_ID to traceId,
-                    "hello" to helloService,
-                ),
-            ).build()
+            builder
+                .graphQLContext(
+                    mapOf(
+                        TRACE_ID to traceId,
+                        "hello" to helloService,
+                    ),
+                ).build()
         }
 
         return chain.next(request)
